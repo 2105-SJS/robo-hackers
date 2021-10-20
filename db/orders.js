aa1cb9d0ef2b3405a0980420db5d6c7599a8d615
Raydan Bala <raydanb@gmail.com>
2021-10-19 21:48:02 -0700

    db/orders.js created, moved createOrder from db/inex.js

---
+++ db/orders.js
@@ -0,0 +1,18 @@
+const { client } = require('./index');
+
+const createOrder = async ({status, userId, datePlaced}) => {
+  try {
+    const {rows: [order]} = await client.query(`
+    INSERT INTO orders(status, "userId", "datePlaced")
+    RETURNING *;
+    `, [status, userId, datePlaced]);
+    return order;
+    
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  createOrder
+}
