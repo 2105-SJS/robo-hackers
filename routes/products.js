e64ad78f9950c57aa8ab69bc29b4342e7ea6c2d5
Amar Pal Singh <amarpal023@gmail.com>
2021-10-20 20:36:23 -0700

    apiroutes. Closes #20

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const productsRouter = express.Router();
-const { getAllProducts } = require('../db');
+const { getAllProducts, getProductById } = require('../db');
 
 productsRouter.get('/', async (req, res, next) => {
     try {
@@ -8,6 +8,18 @@ productsRouter.get('/', async (req, res, next) => {
         res.send(products)
     } catch (error) {
         next (error);
+    };
+});
+
+
+
+products.get('/:productid', async (req, res, next) => {
+    try {
+        const {productId} = req.params;
+        const product = await getProductById(productId);
+        res.send(product)
+    } catch (error) {
+        next (error)
     };
 });
 
