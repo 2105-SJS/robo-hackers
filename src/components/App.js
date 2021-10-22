fb1950797a9f76d284be819e000bfae8a96d6f23
mendez-ed43 <emendez3117@gmail.comu>
2021-10-21 20:06:12 -0700

    Login component created and added. Closes #44

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,7 @@ const App = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword ] = useState('');
   const [token, setToken] = useState('');
+  const [user, setUser] = useState('');
   const [userId, setUserId] = useState('');
   const [loggedIn, setLoggedIn] = useState(false);
 
@@ -55,11 +56,18 @@ const App = () => {
     <div className="App">
       <h1>Hello, World!</h1>
       <h2>{ message }</h2>
+      <Navigation token={token} setToken= {setToken} loggedIn = {loggedIn} setLoggedIn = {setLoggedIn} setUsername = {setUsername} setPassword = {setPassword}/>
       <div>
         <Switch>
+
+          <Route exact path = "/login">
+            <Login setToken= {setToken} token={token} setUsername = {setUsername} setPassword = {setPassword} username = {username} password = {password} setLoggedIn = {setLoggedIn} setUser = {setUser} user = {user} />
+          </Route>
+
           <Route exact path = "/products">
             <Products products = {products} fetchProducts = {fetchProducts} setProducts = {setProducts}/>
           </Route>
+          
         </Switch>
       </div>
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,8 @@ export { default as App } from './App';
 
 export {default as Products } from './Products';
 
-export {default as SingleProduct} from './SingleProduct'
+export {default as SingleProduct} from './SingleProduct';
+
+export {default as Navigation} from './Navigation';
+
+export {default as Login} from './Login';
