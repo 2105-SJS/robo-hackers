438b3e3260d4805c59f559c7e8fc98372f2a1e3b
mendez-ed43 <emendez3117@gmail.comu>
2021-10-21 19:28:29 -0700

    Nav bar created. Closes #38

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as App } from './App';
 export {default as Products } from './Products';
 
 export {default as Singleproduct} from './SingleProduct'
+export {default as Navigation} from './Navigation'
