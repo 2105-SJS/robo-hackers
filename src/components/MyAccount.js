fe715541f5c95df1e0fca3cd1b30fe54495ab80a
Raydan Bala <raydanb@gmail.com>
2021-11-11 20:45:33 -0800

    feat: Hide My Account unless user is logged in with token. Closes #126

--- src/components/MyAccount.js
+++ src/components/MyAccount.js
@@ -4,7 +4,6 @@ const MyAccount = ({ user }) => {
   console.log(user)
   return <>
     <div>Welcome, {user.username}!</div>
-    <img src='https://static01.nyt.com/images/2011/09/28/dining/28CHICKSKIN_SPAN/28CHICKSKIN-articleLarge.jpg?quality=75&auto=webp&disable=upscale'></img>
   </>
 }
 export default MyAccount;
