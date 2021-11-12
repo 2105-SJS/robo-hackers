6c97ac69f00a47120f2a3dbbddb35c11e9a33376
Raydan Bala <raydanb@gmail.com>
2021-11-11 20:31:09 -0800

    feat(fix): Updated users login route to send user info after deleting user pasword. Closes #126

--- src/components/App.js
+++ src/components/App.js
@@ -23,7 +23,6 @@ const App = () => {
   const [token, setToken] = useState('');
   const [user, setUser] = useState('');
   const [loggedIn, setLoggedIn] = useState(false);
-  
   const fetchProducts = async () => {
     try {
       const productsObj = await callAPI({
