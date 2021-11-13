70f3af57760ffbeed13f28d15caec50fe1eb4e4e
Amar Pal Singh <amarpal023@gmail.com>
2021-11-13 13:40:38 -0800

    testting cart and products

--- src/components/index.js
+++ src/components/index.js
@@ -17,3 +17,5 @@ export {default as CheckoutForm} from './CheckoutForm';
 export {default as Checkout} from './Checkout';
 
 export {default as ProductById} from './ProductById';
+
+export {default as Orders} from './Orders';
