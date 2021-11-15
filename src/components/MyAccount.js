013d9c4d0478bb3fd6aa3a5ffc6d1b232e4889dd
Urabe <mendez.f.michael@gmail.com>
2021-11-14 20:19:38 -0800

    front end for add new product. Closes #167

--- src/components/MyAccount.js
+++ src/components/MyAccount.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import AddProduct from './';
 
 const MyAccount = ({ user }) => {
   return <>
     <div>Welcome, {user.username}!</div>
+
+    {
+      isAdmin ? 
+      <AddProduct /> : null
+    }
   </>
 }
 export default MyAccount;
